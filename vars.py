66ada7a20cdf78c61053b732509af94b31734541
jangidudi45 <jangidudit45@gmail.com>
2025-08-13 09:07:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,16 +8,17 @@ API_HASH = environ.get("API_HASH", "93e1b0c387cabe34a3ccfa1724ae8527")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "1368753935"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+CREDIT = environ.get("CREDIT", "Pikachu")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '1368753935').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '1368753935').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7062964338').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
